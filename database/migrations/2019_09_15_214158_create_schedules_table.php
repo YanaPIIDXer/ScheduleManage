a973b76d1c0de1032ac0d411e61f7f3fb3ba668b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-09-16 14:26:54 +0900

    スケジュールテーブルにタイトルカラムを追加。

--- database/migrations/2019_09_15_214158_create_schedules_table.php
+++ database/migrations/2019_09_15_214158_create_schedules_table.php
@@ -19,6 +19,7 @@ class CreateSchedulesTable extends Migration
             $table->date("date");
             $table->time("start_time");
             $table->time("end_time");
+            $table->string("title");
             $table->timestamps();
         });
     }
