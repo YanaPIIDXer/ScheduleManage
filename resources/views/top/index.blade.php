6d3cee785126ac76cb4c7e2ff4ce5f33a259f202
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-09-18 06:17:28 +0900

    トップページのレイアウト修正。

--- resources/views/top/index.blade.php
+++ resources/views/top/index.blade.php
@@ -1,9 +1,11 @@
 @extends("layouts/view")
 
 @section("Content")
-<h1 class="text-center">スケジュール管理システム</h1>
-<div class="text-center">
-    <a href="{{ url('/login') }}">ログイン</a><br />
-    <a href="{{ url('/register') }}">ユーザ登録</a><br />
+<div class="container">
+    <div class="row">
+        <h1 class="col-sm-12 text-center mb-5">スケジュール管理システム</h1>
+        <a class="col-sm-6 offset-sm-3 mb-3 btn btn-success" href="{{ url('/login') }}">ログイン</a><br />
+        <a class="col-sm-6 offset-sm-3 btn btn-primary" href="{{ url('/register') }}">ユーザ登録</a><br />
+    </div>
 </div>
 @endsection
