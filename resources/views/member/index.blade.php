52657098664152eef71dc17caed9fbe1763b0310
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-09-18 06:20:24 +0900

    メンバートップページのレイアウト修正。

--- resources/views/member/index.blade.php
+++ resources/views/member/index.blade.php
@@ -1,8 +1,12 @@
 @extends("layouts/view")
 
 @section("Content")
-<h1>スケジュール管理システム　会員ページ</h1>
-<a href="{{ url('/member/schedule_list') }}">登録スケジュール一覧</a><br />
-<a href="{{ url('/member/add_schedule') }}">スケジュール登録</a><br />
-<a href="{{ url('/logout') }}">ログアウト</a><br />
+<div class="container">
+    <div class="row">
+        <h1 class="col-sm-12 text-center mb-5">スケジュール管理システム　会員ページ</h1>
+        <a class="col-sm-6 offset-sm-3 mb-3 btn btn-success" href="{{ url('/member/schedule_list') }}">登録スケジュール一覧</a><br />
+        <a class="col-sm-6 offset-sm-3 mb-3 btn btn-primary" href="{{ url('/member/add_schedule') }}">スケジュール登録</a><br />
+        <a class="col-sm-6 offset-sm-3 mb-3 btn btn-danger" href="{{ url('/logout') }}">ログアウト</a><br />
+    </div>
+</div>
 @endsection
