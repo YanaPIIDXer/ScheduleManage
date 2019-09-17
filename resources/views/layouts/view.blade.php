0b3c7632829dfbcc8774d365ecc8d871ebe9f95f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-09-18 00:18:28 +0900

    bootstrapを導入。

--- resources/views/layouts/view.blade.php
+++ resources/views/layouts/view.blade.php
@@ -3,6 +3,11 @@
 <head>
 <title>スケジュール管理</title>
 <meta charset="utf-8">
+<meta name="robots" content="noindex,nofollow">
+<link rel="stylesheet" href="{{ asset('/bootstrap/css/bootstrap.min.css') }}">
+<script src="{{ asset('/bootstrap/js/jquery.min.js') }}"></script>
+<script src="{{ asset('/bootstrap/js/popper.min.js') }}"></script>
+<script src="{{ asset('/bootstrap/js/bootstrap.min.js') }}"></script>
 </head>
 <body>
 @if (session("flash_message"))
